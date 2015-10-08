f8fdf5803835102aa2fc06eb346f0f959721809b
Travis Eubanks <tmeubanks113@gmail.com>
2015-10-08 06:40:59 -0500

    adds to book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,8 @@
 class Book < ActiveRecord::Base
    scope :finished, -> { where.not(finished_on: nil) }
    scope :recent,   -> { where('finished_on > ?', 2.days.ago)}
-   scope :search, ->(keyword){where(title: keyword) if keyword.present?}
+   scope :search, ->(keyword){where('keywords LIKE ?', "%#{keyword.downcase}>%") if keyword.present?}
+
 
    before_save :set_keywords
 
@@ -16,4 +17,11 @@ class Book < ActiveRecord::Base
    #       all
    #    end
    # end
+
+
+   protected #this can NOT be access from anywhere else
+   # Takes this Book objects values for title, author and description, downcase it all into new array then join it all in on big text
+      def set_keywords
+         self.keywords = [title, author, description].map(&:downcase).join(' ')
+      end
 end
