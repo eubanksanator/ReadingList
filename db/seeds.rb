37e3ac7e6e94ccc8f51fbc246ea07291efd0806b
Travis Eubanks <tmeubanks113@gmail.com>
2015-10-08 07:34:57 -0500

    adds BookGenre table  for has many through

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,8 @@
 Book.destroy_all
+Genre.destroy_all
+
+fiction = Genre.create!(name: 'Fiction')
+non_fiction = Genre.create!(name: 'Non-fiction')
 
 
 Book.create!([{
@@ -7,7 +11,8 @@ Book.create!([{
   description: "Probably my favorite science fiction book (and series) I've ever read. Hyperion is written in a style similar to The Canterbury Tales, in which a series of stories are told by the main characters. Each story is a gem in itself, but alude to the larger storyline. The scope of the story is ambitious - spanning time, planets religion and love.",
   amazon_id: "0553283685",
   rating: 5,
-  finished_on: 10.days.ago
+  finished_on: 10.days.ago,
+  genre: fiction
 },
 {
   title: "Jony Ive: The Genius Behind Apple's Greatest Products",
@@ -15,7 +20,8 @@ Book.create!([{
   description: "Even though I respect Ive, I felt this biography only hit skin deep. It went over all the major events in his life, his passion for design, awards he achieved -- but that's really it. I dont't feel I know him anymore than before reading this.",
   amazon_id: "159184617X",
   rating: 3,
-  finished_on: 12.days.ago
+  finished_on: 12.days.ago,
+  genre: fiction
 },
 {
   title: "Mindstorms",
@@ -23,7 +29,8 @@ Book.create!([{
   description: "Although this book focuses on the cognitive advantages to having children use technology from an early age, it is also an in depth look at how people can learn for themseves. As someone who was often distracted and bored at times during school, Mindstorms highlights some of the reasoning behind that feeling and what we can do as teachers to help minimize it.",
   amazon_id: "0465046746",
   rating:4,
-  finished_on: nil
+  finished_on: nil,
+  genre: non_fiction
 }])
 
 p "Created #{Book.count} books"
