9fcbe503c804edc02796773af2bed84d2f832910
Travis Eubanks <tmeubanks113@gmail.com>
2015-10-08 06:52:00 -0500

    Book model stable

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,7 @@
 class Book < ActiveRecord::Base
    scope :finished, -> { where.not(finished_on: nil) }
    scope :recent,   -> { where('finished_on > ?', 2.days.ago)}
-   scope :search, ->(keyword){where('keywords LIKE ?', "%#{keyword.downcase}>%") if keyword.present?}
+   scope :search, ->(keyword){ where('keywords LIKE ?', "%#{keyword.downcase}%") if keyword.present?}
 
 
    before_save :set_keywords
