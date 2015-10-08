a1eed3731ff8c01d620a1a595bd0c1fcf5c338d4
Travis Eubanks <tmeubanks113@gmail.com>
2015-10-07 19:21:54 -0500

    Adds scopes for books and creates migration for set_keywords callback

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,19 @@
 class Book < ActiveRecord::Base
    scope :finished, -> { where.not(finished_on: nil) }
    scope :recent,   -> { where('finished_on > ?', 2.days.ago)}
+   scope :search, ->(keyword){where(title: keyword) if keyword.present?}
+
+   before_save :set_keywords
 
    def finished?
       finished_on.present?
    end
+
+   # def self.search(keyword)
+   #    if keyword.present?
+   #       where(title: keyword)
+   #    else
+   #       all
+   #    end
+   # end
 end
